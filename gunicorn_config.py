3ff4c97efaa531d3350845e8b3053ffc1b807cf5
Shazid Nawas Shovon <shazid15-12929@diu.edu.bd>
2025-09-05 03:16:01 +0600

    updated files for backend to deploy in render

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,9 +1,4 @@
-import os
-
-# Get the PORT environment variable or use 8502 as the default
-port = int(os.environ.get("PORT", 8502))
-
-bind = f"0.0.0.0:{port}"
+bind = "0.0.0.0:8502"
 workers = 1
 threads = 8
 timeout = 120
